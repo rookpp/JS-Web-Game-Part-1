e8684198697940aded705521857ceb2b0659f7b6
Kamil Holub <kahol20000@gmail.com>
2023-05-16 18:34:12 -0400

    finished activity

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+console.log(window.innerHeight)
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+for(let j = 0;j<(937/100);j++)
+{
+    for(let i = 0;i<(1920/100);i++){
+        if(j<5){
+            newImage('assets/sky.png',i*100,937-(j*100))
+        }
+        if(j>=5){
+            newImage('assets/grass.png',i*100,937-(j*100))
+        }
+    }
+}
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
